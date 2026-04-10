814af727ab3b51f1a325168580b8167802bc7c9a
ksskarthik18 <karthik.kirla06@gmail.com>
2026-04-10 23:10:23 +0530

    Time: 0 ms (100%), Space: 12.5 MB (8.84%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int> mergearr;
+        for(int i=0;i<m;i++){
+            mergearr.push_back(nums1[i]);
+
+        }
+        for(int j=0;j<n;j++){
+            mergearr.push_back(nums2[j]);
+        }
+        sort(mergearr.begin(),mergearr.end());
+        for(int i=0;i<m+n;i++){
+            nums1[i]=mergearr[i];
+        }
+
+
+        
+    }
+};
