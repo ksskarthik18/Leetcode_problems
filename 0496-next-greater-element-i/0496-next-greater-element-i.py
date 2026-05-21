f0798b434a440358ee910a125fa873328773c5b3
ksskarthik18 <karthik.kirla06@gmail.com>
2026-05-21 08:46:42 +0530

    Time: 6 ms (28.45%), Space: 12.6 MB (57.41%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,29 +1,43 @@
 class Solution(object):
+    def __init__(self):
+        self.stack = []
+    def push(self,value):
+        self.stack.append(value)
+    
+    def pop(self):
+        if self.empty():
+            return "Stack Underflow"
+        return self.stack.pop()
+    
+    def top(self):
+        if self.empty():
+            return -1
+        return self.stack[-1]
+    def size(self):
+        return len(self.stack)
+    def empty(self):
+        return len(self.stack) == 0
+
     def nextGreaterElement(self, nums1, nums2):
         """
         :type nums1: List[int]
         :type nums2: List[int]
         :rtype: List[int]
         """
-
-        stack = []
-        nge = {}
-
-        for num in nums2:
-
-            while stack and num > stack[-1]:
-                nge[stack.pop()] = num
-
-            stack.append(num)
-
-        # Remaining elements have no greater element
-        while stack:
-            nge[stack.pop()] = -1
-
-        result = []
-
+        nge ={}
+        n2 = len(nums2)
+        for i in range(n2-1,-1,-1):
+            while not (self.empty()) and self.top()<= nums2[i]:
+                self.pop()
+            if self.empty():
+                nge[nums2[i]] = -1
+            else:
+                nge[nums2[i]] = self.top()
+            self.push(nums2[i])
+        result =[]
         for num in nums1:
             result.append(nge[num])
-
         return result
+    
+
         
