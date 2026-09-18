ece1a5948fbb66842b54db228b184786a59bea35
ksskarthik18 <karthik.kirla06@gmail.com>
2026-09-18 16:35:16 +0530

    Time: 1 ms (10.05%), Space: 12.4 MB (22%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        dp = [-1]*n
+
+        def solve(i):
+            if i < 0:
+                return 0
+            if i == 0:
+                return nums[i]
+            
+            if dp[i]!= -1:
+                return dp[i]
+
+            pick = nums[i] + solve(i-2)
+            not_pick = solve(i-1)
+
+            dp[i] = max(pick,not_pick)
+
+            return dp[i]
+        
+        return solve(n-1)
+
+    
+        
